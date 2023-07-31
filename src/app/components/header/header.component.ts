a7b8bdfd1c965cd09f81b3bcc795e341c0ec9b4c
Marcos <m.g4089188@gmail.com>
2023-07-31 19:13:18 -0300

    header animations / hovers

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,28 @@
 import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { faUser, faPaw, faQrcode, faPrint, faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
+import { User } from 'src/app/shared/interfaces/user.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements AfterViewInit {
+  @ViewChildren('headerItem') headerItems!: QueryList<ElementRef>;
+
   faUser = faUser;
   faPaw = faPaw;
   faQrcode = faQrcode;
   faPrint = faPrint;
   faMagnifyingGlassLocation = faMagnifyingGlassLocation;
 
-  @ViewChildren('headerItem') headerItems!: QueryList<ElementRef>;
+  user: User|null;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+    this.user = this.authService.getUser();
+  }
 
   ngAfterViewInit() {
     this.animateItems();
