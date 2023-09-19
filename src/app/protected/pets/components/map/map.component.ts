751101c37497589309fe84b4dce2f5a84cf4df3a
Marcos <m.g4089188@gmail.com>
2023-09-19 10:14:25 -0300

    pet profile map fixes

--- src/app/protected/pets/components/map/map.component.ts
+++ src/app/protected/pets/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import * as L from 'leaflet';
 
 @Component({
@@ -7,15 +8,19 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  faExclamationCircle = faExclamationCircle;
+
   @Input() address!: string;
   @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
   map!: L.Map;
+  unknowError: boolean = false;
+  errorMessage!: string;
 
   ngOnInit(): void {
     const mapElement = this.mapContainer.nativeElement;
 
     // Inicializa el mapa en el contenedor especificado
-    this.map = L.map(mapElement).setView([51.505, -0.09], 50);
+    this.map = L.map(mapElement, {zoomControl: false}).setView([51.505, -0.09], 50);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -25,23 +30,25 @@ export class MapComponent implements OnInit {
     fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(this.address)}`)
       .then(response => response.json())
       .then(data => {
+        this.unknowError = false;
         if (data.length > 0) {
           const lat = parseFloat(data[0].lat);
           const lon = parseFloat(data[0].lon);
           const location = L.latLng(lat, lon);
 
           L.circle(location, {
-            color: 'blue', // Color del círculo
-            fillColor: 'blue', // Color de relleno del círculo
-            fillOpacity: 0.3, // Opacidad del relleno
-            radius: 2000 // Radio del círculo en metros (ajusta según tus necesidades)
+            color: 'blue',
+            fillColor: 'blue',
+            fillOpacity: 0.3,
+            radius: 2000
           }).addTo(this.map);
 
-          this.map.setView(location, 13);
+          this.map.setView(location, 12);
         }
       })
       .catch(error => {
-        console.error(error);
+        this.unknowError = true;
+        this.errorMessage = 'Ocurró un error al obtener el mapa.';
       });
   }
 }
